b403a9eeac05dd5befb20e0260a120404caa8c0c
Yana Spiridonova <iana.spiridonova@bk.ru>
2023-02-20 02:22:33 +0100

    bug with async try and catch

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ let jewerlyCardTemplate = document.querySelector('#jewerly-card');
 let itemBox = document.querySelector('.item-box');
 let errorTempl = document.querySelector('#errors-template');
 
-fetch('http://fakestoreapi.com/products/category/jewelery')
-  .then(response => response.json())
-  .then((data) => {
+async function getJewerlyInf() {
+  try {
+    let response = await fetch ('http://fakestoreapi.com/products/category/jewelery');
+    let data = await response.json();
+
     for (let idx of data) {
       let cloneTemplate = jewerlyCardTemplate.content.cloneNode(true);
       let cardTitle = cloneTemplate.querySelector('.card-information_title');
@@ -21,12 +23,40 @@ fetch('http://fakestoreapi.com/products/category/jewelery')
       cardRating.innerText += idx.rating.count;
 
       itemBox.appendChild(cloneTemplate);
-      
-    }
-    console.log(data);
-  })
-  .catch(() => {
+    }   
+  } catch (err) {
     let newErr = errorTempl.content.cloneNode(true);
     itemBox.appendChild(newErr);
-  }) 
+  }
+}
+
+
+
+// fetch('http://fakestoreapi.com/products/category/jewelery')
+//   .then(response => response.json())
+//   .then((data) => {
+//     for (let idx of data) {
+//       let cloneTemplate = jewerlyCardTemplate.content.cloneNode(true);
+//       let cardTitle = cloneTemplate.querySelector('.card-information_title');
+//       let cardImage = cloneTemplate.querySelector('.card-information_picture');
+//       let cardDescription = cloneTemplate.querySelector('.card-information_description');
+//       let cardPrice = cloneTemplate.querySelector('.card-information_price');
+//       let cardRating = cloneTemplate.querySelector('.card-information_rating');
+
+//       cardTitle.innerText = idx.title;
+//       cardImage.src = idx.image;
+//       cardDescription.innerText = idx.description;
+//       cardPrice.innerText = idx.price + ' $';
+//       cardRating.innerText = idx.rating.rate + '☆' + ' ';
+//       cardRating.innerText += idx.rating.count;
+
+//       itemBox.appendChild(cloneTemplate);
+      
+//     }
+//     console.log(data);
+//   })
+  // .catch(() => {
+  //   let newErr = errorTempl.content.cloneNode(true);
+  //   itemBox.appendChild(newErr);
+  // }) 
   
